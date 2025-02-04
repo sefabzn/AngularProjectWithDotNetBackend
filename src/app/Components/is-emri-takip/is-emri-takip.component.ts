f9281a4d69d8ddcf40c4ef2d040893ae437b7699
sefabzn <sefa_bazan_The12@hotmail.com>
2025-02-04 16:48:52 +0300

    Integrate GenelDizaynService into IsEmriTakipComponent; fetch and map Genel Dizayn data to İş Emri list

--- src/app/Components/is-emri-takip/is-emri-takip.component.ts
+++ src/app/Components/is-emri-takip/is-emri-takip.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IsEmriService } from 'src/app/Services/is-emri.service';
+import { GenelDizaynService } from 'src/app/Services/genel-dizayn.service';
 import { IsEmriModel } from 'src/app/Models/isEmri';
+import { GenelDizaynBase } from 'src/app/Models/genelDizaynBase';
 
 @Component({
   selector: 'app-is-emri-takip',
@@ -9,22 +11,52 @@ import { IsEmriModel } from 'src/app/Models/isEmri';
 })
 export class IsEmriTakipComponent implements OnInit {
   isEmriList: IsEmriModel[] = [];
+  genelDizaynList: GenelDizaynBase[] = [];
 
-  constructor(private isEmriService: IsEmriService) { }
+  constructor(
+    private isEmriService: IsEmriService,
+    private genelDizaynService: GenelDizaynService
+  ) { }
 
   ngOnInit(): void {
     this.getIsEmriList();
+    this.getGenelDizaynList();
   }
 
   getIsEmriList() {
     this.isEmriService.getAll().subscribe(response => {
       if (response.success) {
         this.isEmriList = response.data;
+        this.mapTurToIsEmri();
       } else {
         console.error(response.message);
       }
     }, error => {
       console.error('An error occurred while fetching İş Emri data', error);
     });
+  }
+
+  getGenelDizaynList() {
+    this.genelDizaynService.getAll().subscribe(response => {
+      if (response.success) {
+        this.genelDizaynList = response.data;
+        this.mapTurToIsEmri();
+      } else {
+        console.error(response.message);
+      }
+    }, error => {
+      console.error('An error occurred while fetching Genel Dizayn data', error);
+    });
+  }
+
+  mapTurToIsEmri() {
+    if (this.isEmriList.length > 0 && this.genelDizaynList.length > 0) {
+      this.isEmriList.forEach(isEmri => {
+        const genelDizayn = this.genelDizaynList.find(gd => gd.id === isEmri.genelDizaynId);
+        if (genelDizayn) {
+          isEmri.tur = genelDizayn.tur;
+        }
+      });
+    }
   }
 }
